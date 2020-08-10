549d81f662d469b93415e159ce30e0a854b47705
nibble0101 <mj.mawa@yahoo.com>
2020-08-10 11:56:14 +0300

    Added another solution to binary search challenge

--- binary-search/binary-search-algorithm.js
+++ binary-search/binary-search-algorithm.js
@@ -20,6 +20,29 @@ var doSearch = function (array, targetValue) {
   return guess;
 };
 
+/*
+More efficient solution
+
+var doSearch = function(array, targetValue) {
+	var min = 0;
+	var max = array.length - 1;
+    var guess;
+    while(min <= max){
+        guess = Math.floor((min + max)/2);
+        if(array[guess] === targetValue){
+            return guess;
+        }else if(array[guess] < targetValue){
+            min = guess + 1;
+        }else{
+            max = guess - 1;
+        }
+    }
+	return -1;
+};
+
+
+*/
+
 var primes = [
   2,
   3,
